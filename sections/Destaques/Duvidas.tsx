5d469540edc01fcd9c1966aec17e37145132efc9
omattaeus <gomesduartedev@gmail.com>
2024-10-09 14:15:58 -0300

    correcao icone whatsapp secao duvidas

--- sections/Destaques/Duvidas.tsx
+++ sections/Destaques/Duvidas.tsx
@@ -67,20 +67,20 @@ export default function Section({
         <p className="pb-[32px] text-[28px] text-sacramentoState text-sans font-medium text-center">{title}</p>
       </div>
 
-      <div className="pb-24 flex flex-col rounded-xl items-center justify-center space-y-4">
+      <div className="pb-24 px-4 flex flex-col rounded-xl items-center justify-center space-y-4">
         {button?.map((btn, index) => (
           <div key={index} className="w-full flex justify-center">
             <a
               href={btn.url}
               target="_blank"
               rel="noopener noreferrer"
-              className={`w-max px-[22px] py-3 flex-none text-sans text-base font-semibold ${
+              className={`w-full px-4 py-3 flex-none text-sans text-base font-semibold ${
                 index === 0
-                  ? 'bg-transparent border-[1px] border-sacramentoState rounded-xl text-sacramentoState'
-                  : 'bg-sacramentoState rounded-xl text-secondary pl-4 pr-4 flex items-center'
+                  ? 'bg-transparent border-[1px] border-sacramentoState rounded-xl text-center text-sacramentoState'
+                  : 'bg-sacramentoState rounded-xl text-secondary flex items-center pr-2.5 justify-center'
               }`}
             >
-              {btn.name}
+              <span className="text-center">{btn.name}</span>
               {index === 1 && btn.iconButton && (
                 <img src={btn.iconButton} alt="WhatsApp Icon" className="w-6 h-6 ml-2.5" />
               )}
