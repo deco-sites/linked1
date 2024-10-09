0f9d14952a58004f81fdb9153ac5eeb125ac409c
omattaeus <gomesduartedev@gmail.com>
2024-10-09 13:53:46 -0300

    adicionando estilizacao secao duvidas

--- sections/Destaques/Duvidas.tsx
+++ sections/Destaques/Duvidas.tsx
@@ -1,6 +1,24 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
+export type Link = {
+  /**
+   * @title Nome
+   * @description Nome visível para clique
+   */
+  name: string;
+  /**
+   * @title Destino
+   * @description Insira a url ou o caminho de destino
+   */
+    url: string;
+  /**
+   * @title Ícone
+   * @description URL do ícone opcional
+   */
+  iconButton?: ImageWidget;
+};
+
 export interface Props {
     /**
      * @title Ícone de Dúvida
@@ -12,22 +30,65 @@ export interface Props {
      * @description Escreva o título do bloco
      */
     title: string;
+    /**
+     * @title Botão
+     * @description Insira o botão
+     */
+    button?: Link[];
 }
 
 export default function Section({ 
     icon = "", 
-    title = "", 
+    title = "",
+    button = [
+      {
+        name: "Descubra o plano ideal",
+        url: "/planos",
+      },
+      {
+        name: "Agende uma demonstração",
+        url: "/",
+        iconButton: "",
+      },
+    ],
 }: Props) {
 
   return (
-    <div>
-        {icon && (
-              <Image
-                class=""
-                src={icon || ""}
-              />
-        )}
-        <p>{title}</p>
-    </div>
+    <section className="bg-mintCream">
+      <div className="">
+        <div className="pt-[96px] pb-4 flex justify-center">
+          {icon && (
+                <Image
+                  class="w-[32px] h-[32px]"
+                  src={icon || ""}
+                />
+          )}
+        </div>
+        <p className="pb-[32px] text-[28px] text-sacramentoState text-sans font-medium text-center">{title}</p>
+      </div>
+
+      <div className="pb-24 flex flex-col rounded-xl items-center justify-center space-y-4">
+        {button?.map((btn, index) => (
+          <div key={index} className="w-full flex justify-center">
+            <a
+              href={btn.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`w-max px-[22px] py-3 flex-none text-sans text-base font-semibold ${
+                index === 0
+                  ? 'bg-transparent border-[1px] border-sacramentoState rounded-xl text-sacramentoState'
+                  : 'bg-sacramentoState rounded-xl text-secondary pl-4 pr-4 flex items-center'
+              }`}
+            >
+              {btn.name}
+              {index === 1 && btn.iconButton && (
+                <img src={btn.iconButton} alt="WhatsApp Icon" className="w-6 h-6 ml-2.5" />
+              )}
+            </a>
+          </div>
+        ))}
+      </div>
+    </section>
+    
   );
 }
--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,44 +1,71 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
+export type Link = {
+  /**
+   * @title Nome
+   * @description Nome visível para clique
+   */
+  name: string;
+  /**
+   * @title Destino
+   * @description Insira a URL ou o caminho de destino
+   */
+  url: string;
+};
+
 export interface Props {
   /**
-   * @title Logo
-   * @description Insira o logo
+   * @title Menu de Navegação
+   * @description Insira os links do menu de navegação
+   * 
    */
-  logo?: ImageWidget;
+  linkedText: string;
+  /**
+   * @title URL do Link de Texto
+   * @description URL do link adicional
+   */
+  linkedUrl: string;
+  /**
+   * @title Endereço
+   * @description Representa o endereço físico associado à entidade.
+   */
+  address: string;
   /**
    * @title Direitos Reservados
    * @description Texto de direitos reservados
    */
   rightsText: string;
   /**
-   * @title Endereço
-   * @description Insira o endereço do rodapé
+   * @title Link de Texto
+   * @description Insira o texto para o link adicional
    */
-  address: string;
 }
 
 export default function Footer({
-  logo = "", 
+
+  linkedText = "",
+  linkedUrl = "#",
   address = "",
   rightsText = "",
+
 }: Props) {
   return (
-    <footer className="footer">
-      <div className="footer-top">
-        <nav className="footer-menu">
-          {logo && (
-                <Image
-                  class=""
-                  src={logo || ""}
-                />
-          )}
-        </nav>
-      </div>
-      <div className="footer-bottom">
-        <p className="footer-rights">{rightsText}</p>
-        <p className="footer-address">{address}</p>
+    <footer className="bg-sacramentoState">
+      <div className="w-full pt-[159px] text-center">
+        <div className="text-center">
+          <a href={linkedUrl} className="text-center text-[60px] text-caribbeanGreen">
+            {linkedText}
+          </a>
+        </div>
+        <p className="pt-[22px] pb-[66px] font-light lg:text-xs text-center text-mintCream">
+          {rightsText}
+        </p>
+        <div className="pb-[72px] border-t-2 border-white-20 w-full">
+          <p className="pt-[38px] sm:text-[11px] lg:text-xs text-center text-mintCream">
+            {address}
+          </p>
+        </div>
       </div>
     </footer>
   );
