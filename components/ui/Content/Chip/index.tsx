849ae66403caad0709c3c2e989ea8d223fb4ea90
omattaeus <gomesduartedev@gmail.com>
2025-01-14 05:21:01 -0300

    fazendo hover em chips (categorias)

--- components/ui/Content/Chip/index.tsx
+++ components/ui/Content/Chip/index.tsx
@@ -1,31 +1,39 @@
 import type { JSX } from "preact";
-
 import Image from "apps/website/components/Image.tsx";
 
 function Chip({
   name,
   url,
   icon,
+  hiddenImage,
+  colorTheme,
 }: JSX.IntrinsicElements["div"] & Props) {
   return (
-    <>
-      <div className="w-fit rounded-[58px] inline-flex px-4 py-3 bg-mintCream font-sans font-medium text-primary items-center">
-        <div className="inline-flex items-center">
-          <Image
-            className="w-4 h-4"
-            src={icon || ""}
-            alt={name}
-            width={10.67}
-          />
-          <a
-            href={url}
-            className="text-md md:text-lg pl-2 pr-[1.5px] font-sans font-medium leading-tight-18"
-          >
-            {name}
-          </a>
-        </div>
+    <div className="relative w-fit inline-flex items-center group">
+      <div className="px-4 py-3 flex gap-x-2 items-center bg-mintCream text-sacramentoState hover:lg:bg-sacramentoState hover:lg:text-darkMintCream rounded-[58px] z-10 transition-all duration-300 ease-in-out">
+        <Image
+          className="w-4 h-4"
+          src={icon || ""}
+          alt={name}
+          width={10.67}
+        />
+        <a
+          href={url}
+          className="text-md md:text-lg font-sans font-medium leading-tight-18"
+        >
+          {name}
+        </a>
       </div>
-    </>
+      {hiddenImage && (
+        <div className="hidden lg:block w-[374px] h-[374px] absolute -top-10 left-1/2 transform -translate-x-1/2 opacity-0 scale-75 pointer-events-none group-hover:opacity-100 group-hover:scale-100 transition-all duration-300 ease-out">
+          <Image
+            src={hiddenImage.hoverImage || ""}
+            alt={`Imagem para ${name}`}
+            className="w-full h-full object-cover rounded-full"
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
--- components/ui/Galery/GaleryContent/index.tsx
+++ components/ui/Galery/GaleryContent/index.tsx
@@ -5,22 +5,57 @@ function GaleryContent({ items }: JSX.IntrinsicElements["div"] & Props) {
   return (
     <>
       <div className="px-4 lg:px-10 pb-[59px] overflow-x-hidden">
-        <div ref={(el) => (emblaRef.value = el)} className="embla w-full">
+        <div className="embla w-full">
           <div className="embla__container flex md:grid md:grid-cols-4 gap-x-4">
             {items.map((item, index) => (
               <div
-                className="embla__slide flex flex-col flex-shrink-0 border-t-2 border-white-20 max-w-[80%] md:max-w-[100%]"
+                className="embla__slide group flex flex-col flex-shrink-0 border-t-2 border-white-20 hover:md:border-white group-hover:border-white max-w-[80%] md:max-w-[100%] transition-all duration-700 ease-out"
                 key={index}
               >
                 <p className="pt-[23px] text-base text-white opacity-80 text-left">
                   {item.title}
                 </p>
-                <div className="flex justify-center my-[26px] px-0 h-[486px] relative">
+                <div className="flex justify-center my-[26px] px-0 h-[486px] relative group">
                   <Image
                     className="absolute w-full h-full object-cover rounded-2xl"
                     src={item.image || ""}
                     width={315}
                   />
+                  <div className="hidden lg:block absolute bottom-0 mx-2 mb-[11px] left-0 right-0 opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-700 ease-out">
+                    <div className="grid gap-y-1">
+                      {item.order?.map((order, idx) => (
+                        <div
+                          key={idx}
+                          className="py-[13px] pl-[19px] bg-sacramentoState-80 bg-opacity-10 backdrop-blur-[2.4px] rounded-[6.4px]"
+                        >
+                          <div className="text-start grid gap-y-1">
+                            <div className="flex items-center gap-x-[3px]">
+                              <p className="text-[9.6px] font-mono font-light text-white/80">
+                                {order.descriptionOrder}
+                              </p>
+                              <Image
+                                src={order.image}
+                                width={12.8}
+                                height={12.8}
+                                className="w-[12.8px] h-[12.8px]"
+                              />
+                            </div>
+                            <p className="text-[11.2px] font-sans font-light leading-tight text-white">
+                              {order.subtitleOrder}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                    {item.SectionImage && (
+                      <div>
+                        <Image
+                          className="w-full h-full object-cover rounded-[6.4px]"
+                          src={item.SectionImage.image}
+                        />
+                      </div>
+                    )}
+                  </div>
                 </div>
                 <div className="w-full pl-0 text-left">
                   <p className="pl-[3px] text-base text-mintCream opacity-80">
